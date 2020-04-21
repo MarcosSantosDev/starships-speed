f968a26e04eaea63daed74df95a561ce6ee5eff5
marcos santos <marcos.santos@alock.com.br>
2020-04-21 14:24:03 -0300

    create header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+        <header className="header">
+          <h3>STARSHIPS SPEED</h3>
+        </header>
+    );
+  }
+}
